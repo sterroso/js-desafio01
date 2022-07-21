d0557b3a69118c504d6930a76acfc0c2d9faeec4
Sergio Terroso <sergio.terroso@gmail.com>
2022-07-20 20:17:40 -0500

    Validaciones

--- js/desafio01.js
+++ js/desafio01.js
@@ -7,6 +7,14 @@ const execButton = document.getElementById('execButton');
 // Botón [ Limpiar Tablas ]
 const clearButton = document.getElementById('clearButton');
 
+// Valores mínimo y máximo de amplitud
+const MIN_AMPLITUD = -10;
+const MAX_AMPLITUD = 10;
+
+// Valores mínimo y máximi de profundidad
+const MIN_PROFUNDIDAD = 0;
+const MAX_PROFUNDIDAD = 10;
+
 // Multiplicador de cada tabla de multiplicar (0, 1, 2, ..., n)
 let multiplicador = 0
 
@@ -20,17 +28,86 @@ const paramValues = {
     profundidad: 10
 };
 
+// Una función para validar los argumentos pasados para amplitud (inferior y superior)
+const isValidAmplitud = input => {
+    // Intenta convertir la entrada en un entero con la función parseInt
+    let retval = parseInt(input);
+
+    // Si retval no es NaN, no es nulo, es mayor o igual a MIN_AMPLITUD y es menor 
+    // o igual a MAX_AMPLITUD, devuelve verdadero (y allí sale de la función)
+    if (!isNaN(retval) && retval != null && retval >= MIN_AMPLITUD && retval <= MAX_AMPLITUD) return true;
+
+    // Si llega hasta aquí es porque alguna de las condiciones anteriores no se cumplió
+    return false;
+}
+
+// Una función para validad la profundidad
+const isValidProfundidad = input => {
+    // Intenta convertir la entrada en un entero
+    let retval = parseInt(input);
+
+    // Si retval no es NaN, no es null, es mayor o igual a MIN_PROFUNDIDAD y es menor
+    // o igual a MAX_PROFUNDIDAD, devuelve verdadero (y allí sale de la función)
+    if (!isNaN(retval) && retval !== null & retval >= MIN_PROFUNDIDAD && retval <= MAX_PROFUNDIDAD) return true;
+
+    // Si llega hasta aquí es porque alguna de las condiciones anteriores no se cumplió
+    return false;
+}
+
 execButton.addEventListener('click', evt => {
     // Cargar los valores de amplitud y profundidad en el objeto contenedor paramValues:
 
-    // paramValues.amplitudInferior: la tabla inferior que se va a calcular.
-    paramValues.amplitudInferior = Number(document.getElementById('rangoLimiteInferior').value);
+    // Primero recupero la cadena de texto que se encuentra en el campo #rangoLimiteInferior de mi página.
+    let stringRangoLimiteInferior = document.getElementById('rangoLimiteInferior').value;
 
-    // paramValues.amplitudSuperior: la tabla superior que se va a calcular.
-    paramValues.amplitudSuperior = Number(document.getElementById('rangoLimiteSuperior').value);
+    // Si stringRangoLimiteInferior es válido...
+    if (isValidAmplitud(stringRangoLimiteInferior)) {
+        // ... se convierte a entero y el resultado se guarda en
+        // la propiedad amplitudInferior del objeto paramValues.
+        paramValues.amplitudInferior = parseInt(stringRangoLimiteInferior);
+    } else {
+        // ... de lo contrario se muestra un mensaje al usuario
+        alert(`El Límite inferior de la Amplitud debe ser un entero entre ${MIN_AMPLITUD} y ${MAX_AMPLITUD} inclusive.`);
+        // Termina la ejecución del escuchador del evento 'click' devolviendo un false.
+        return false;
+    }
 
-    // paramValues.profundidad: el entero máximo por el que se va a multiplicar cada tabla.
-    paramValues.profundidad = Number(document.getElementById('profundidad').value);
+    // Luego recupero la cadena de texto que se encuentra en el campo #rangoLimiteSuperior de mi página.
+    let stringRangoLimiteSuperior = document.getElementById('rangoLimiteSuperior').value;
+
+    // Si stringRangoLimiteInferior es válido...
+    if (isValidAmplitud(stringRangoLimiteSuperior)) {
+        // se convierte a entero y el resultado se guarda en la propiedad amplitudInferior del objeto paramValues.
+        paramValues.amplitudSuperior = parseInt(stringRangoLimiteSuperior);
+
+        // Si el valor de paramValues.amplitudSuperior es menor al valor de paramValues.amplitudInferior...
+        if(paramValues.amplitudSuperior < paramValues.amplitudInferior) {
+            // Notifica al usuario que el valor superior debe ser mayor al valor inferior
+            alert('El valor de Límite Superior de la Amplitud debe ser mayor al valor de Límite Inferior de la Amplitud');
+            // Termina la ejecución del escuchador del evento 'click' devolviendo un false.
+            return false;
+        }
+    } else {
+        // ... de lo contrario se muestra un mensaje al usuario
+        alert(`El Límite superior de la Amplitud debe ser un entero entre ${MIN_AMPLITUD} y ${MAX_AMPLITUD} inclusive.`);
+        // Termina la ejecución del escuchador del evento 'click' devolviendo un false.
+        return false;
+    }
+
+    // Finalmente, recupero la cadena de texto que se encuentra en el campo #profundidad de mi página.
+    let stringProfundidad = document.getElementById('profundidad').value;
+
+    // Si stringProfundidad es válido ...
+    if(isValidProfundidad(stringProfundidad)) {
+        // ... se convierte a entero y el resultado se guarda en la propiedad
+        // profundidad del objeto paramValues
+        paramValues.profundidad = parseInt(stringProfundidad);
+    } else {
+        // ... de lo contrario se muestra un mensaje al usuario
+        alert(`La profundidad debe ser un entero entre ${MIN_PROFUNDIDAD} y ${MAX_PROFUNDIDAD} inclusive.`);
+        // Termina la ejecución del escuchador del evento 'click' devolviendo un false.
+        return false;
+    }
 
     // Se limpia cualquier contenido anterior del área de salida.
     limpiarSalida();
@@ -44,7 +121,7 @@ clearButton.addEventListener('click', evt => {
     limpiarSalida();
 });
 
-function multiplicar(params) {
+const multiplicar = params => {
     for(let i = params.amplitudInferior; i <= params.amplitudSuperior; i++) {
         // Por cada tabla crea un div contenedor ...
         let contenedorTabla = outputContainer.appendChild(document.createElement('div'));
